2db6ffa1e8c0eebdc9faf2498dd0d0de387e662c
Javaboiiii <devrajlokhande1610@gmail.com>
2024-06-29 14:23:35 +0530

    Schema Definitin done for the issue

---
+++ src/server/Issue.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const IssueSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        required: true
+    },
+    email: {
+        type: String, 
+        required: true
+    },
+    phone: {
+        type: String, 
+        required: true
+    },
+    problem: {
+        type: String, 
+        required: true
+    }
+})
+
+mongoose.model('Issue', IssueSchema)
